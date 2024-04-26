652fa4d78c293f7c6b725289427587f85fcb03b3
Abraão Novak <61474289+AbraaoNovak@users.noreply.github.com>
2024-04-25 22:31:08 -0300

    post.js

--- Script/post.js
+++ Script/post.js
@@ -1,14 +1,14 @@
 document.querySelector('.send-btn').addEventListener('click', function(event) {
     axios.post('https://2e06-2804-7f0-b341-f603-5b44-8735-5c57-ced4.ngrok-free.app/api/onboarding', {
-     "fullName": "Fernando Dias",
-        "emailTip": "fernadno.dias@jemsystems.com",
-        "leader": "Fernando Dias",
-        "position": "Web dev",
-        "location":"Local",
+     "fullName": "Abraão Novak",
+        "emailTip": "abraao.novak@jemsystems.com",
+        "leader": "Eu mermo",
+        "position": "Severino",
+        "location":"Joinville",
         "officeLocation": "Joinville",
         "department": "IT",
         "equipment": "DELL LATITUDE 3540",
-        "date": "2024-04-25",
+        "date": "2024-12-24",
         "peripherals": [
             "mouse"
         ]
